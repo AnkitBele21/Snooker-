bae07195ba172d4b82f1a34bdc55381c9a7fe9b1
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-12-18 09:25:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+const apiKey = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
+const sheetId = '1MdV4NpeuadDczDGOS7peYTFk7P3NGeZJQo20GzEjQtc';
+const range = 'A:A'; // Assuming you want to fetch all data from column A
+
+function fetchClubNames() {
+    gapi.client.init({
+        'apiKey': apiKey,
+    }).then(function() {
+        return gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: sheetId,
+            range: range,
+        });
+    }).then(function(response) {
+        var range = response.result;
+        if (range.values.length > 0) {
+            appendClubNames(range.values);
+        } else {
+            console.log('No data found.');
+        }
+    }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+    });
+}
+
+function appendClubNames(clubs) {
+    const dropdown = document.getElementById('partnerClubsDropdown');
+    clubs.forEach(function(club) {
+        let a = document.createElement('a');
+        a.textContent = club[0]; // Assuming the club name is in the first column
+        dropdown.appendChild(a);
+    });
+}
+
+gapi.load('client', fetchClubNames);
+
 document.getElementById("partnerClubsBtn").addEventListener("click", function() {
     document.getElementById("partnerClubsDropdown").classList.toggle("show");
 });
-
-// Fetch club names from a spreadsheet
-// This is a placeholder function. You'll need to implement the actual data fetching logic
-function fetchClubNames() {
-    // Fetch data from the spreadsheet
-    // For example, using Google Sheets API or a similar service
-    // Populate the 'partnerClubsDropdown' div with club names
-}
-
-// Call the function to populate the dropdown on page load
-fetchClubNames();
